a50447df5ec3fea352dc5d36430666bfe7a6930d
jezrel <Jezrel_talledo2100@hotmail.com>
2022-05-28 21:52:38 -0500

    ojalaa

---
+++ js/carrusel.js
@@ -0,0 +1,12 @@
+/*var img = ["1.png", "2.png", "3.png"];
+var c = 0;
+var t;
+var baner = document.getElementById("baner1");
+
+function rotacion() {
+    baner.setAttribute("src", "img/CARTERASFOTOS/" + img[c]);
+    c++;
+    if (c >= 3) c = 0;
+    t = setTimeout("rotacion()", 1000);
+}
+rotacion();*/
