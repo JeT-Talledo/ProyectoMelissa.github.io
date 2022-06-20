9dd4f576daac5ad7c65bfcbdf7c344981bc1d26e
kenny2602 <kenny4602@gmai.com>
2022-06-20 14:48:22 -0500

    ultimo

---
+++ js/codigo.js
@@ -0,0 +1,9 @@
+function mostrar() {
+    document.getElementById('titunoveyfavf').style.display = 'block';
+    document.getElementById('titunoveyfavn').style.display = 'none';
+}
+
+function mostraras(){
+    document.getElementById('titunoveyfavf').style.display = 'none';
+    document.getElementById('titunoveyfavn').style.display = 'block';
+}
